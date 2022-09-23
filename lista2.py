bc3f4dd83d0534372beb079b4214f0add891b717
Darusi Jázmin Bíborka <darusij21@ADY-NAGYATAD.SULINET.HU>
2022-09-23 14:01:45 +0200

    ye

---
+++ lista2.py
@@ -0,0 +1,26 @@
+szavak=["ló", "bakfitty", "csutora",]
+szamok=[1,0,3000,2]
+
+print(szavak)
+
+szavak.sort()
+print(szavak)
+
+szavak.append("hát")
+print(szavak)
+
+szavak.insert(1,"az hogy")
+print(szavak)
+
+szamok.append(20)
+szamok.append(20)
+
+darabZU=szamok.count(20)
+
+print(darabZU)
+
+szamok.remove(20)
+print(szamok)
+
+for szam in szamok:
+    print("szám: "+str(szam))
