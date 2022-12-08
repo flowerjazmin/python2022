90baf8810dae42dc517daaa9cb1d45e6c8836247
flowerjazmin <98027537+flowerjazmin@users.noreply.github.com>
2022-12-08 13:18:28 +0100

    Add files via upload

--- randomszamproba1205.py
+++ randomszamproba1205.py
@@ -1,6 +1,7 @@
 import random
 import math
 
+
 lista=[]
 for i in range(10):
     szam= random.random()
@@ -13,3 +14,23 @@ for i in range(10):
     lista.append(random.randint(10,99))
 
 print(lista)
+
+
+print(random.randint(-1000,1000)*3)
+lista=[]
+for i in range(100):
+    lista.append(random.randint(-1000,1000)*3)
+
+print(lista)
+
+print(sum(lista))
+tutel=[]
+for e in lista:
+    if e%5==0:
+        tutel.append(e)
+
+print(tutel)
+
+tutel=[e//15 for e in lista if e%5==0]
+
+print(tutel)
