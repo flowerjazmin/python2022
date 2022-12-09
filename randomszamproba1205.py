2280686fc22e3d5c56cbf8f001725757b5470979
flowerjazmin <98027537+flowerjazmin@users.noreply.github.com>
2022-12-09 08:44:06 +0100

    Add files via upload

--- randomszamproba1205.py
+++ randomszamproba1205.py
@@ -34,3 +34,27 @@ print(tutel)
 tutel=[e//15 for e in lista if e%5==0]
 
 print(tutel)
+
+
+#hf pótlás
+
+print(random.randrange(167,1667,2)*6)
+
+#randinttel
+
+print((random.randint(83,832)*2+1) * 6)
+
+
+
+
+szavak=["alma", "körte", "barack", "banán", "dinnye", "szőlő"]
+
+#random.seed(1)
+print(szavak[random.randint(0,len(szavak)-1)])
+
+print(random.choice(szavak))
+
+
+
+
+
