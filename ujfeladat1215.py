97c2bcbe7261d14e18af81a84f3e9b8eea1e4618
flowerjazmin <98027537+flowerjazmin@users.noreply.github.com>
2022-12-16 13:20:10 +0100

    Add files via upload

--- ujfeladat1215.py
+++ ujfeladat1215.py
@@ -19,6 +19,43 @@ for e in lista:
     print("*"*(e*egyseg))
 
 
+#3 jegyű szám bekérés
+
+szam=[]
+
+while len(szam)!=3:
+    szam=input("Kérek egy 3 jegyű számot: ")
+
+szam=int(szam)
+
+if szam%12==0:
+    print("Osztható.")
+
+
+print(szam)
+
+
+szoveg="Lorem ipsum dolor sit amet, cock consectetur adipiscing elit. Nunc id gravida mauris, eu tempor dui. Ut convallis et leo et auctor. Integer lacinia porta sapien vel rutrum. Mauris lacinia a justo in sagittis. Proin orci justo, vehicula at accumsan vitae, accumsan a augue. Maecenas hendrerit ipsum eu malesuada scelerisque. Nullam tempor purus sed ante iaculis condimentum. Ut convallis nulla rhoncus metus aliquam sagittis. Sed faucibus, arcu nec vulputate pulvinar, purus neque molestie massa, vel maximus sapien dolor in lacus. Donec interdum, augue at faucibus convallis, nisl mi mollis lectus, in facilisis lacus lacus placerat libero. Ut mollis at turpis hendrerit efficitur. Phasellus vel mollis lacus. Maecenas luctus posuere sapien non aliquet. Pellentesque quis ultricies felis, quis molestie tellus. Fusce vel nunc ac dolor rutrum gravida."
+
+betu="c"
+
+
+
+print(len(szoveg.split(" ")))
+
+szoveg2=szoveg.replace(betu, betu.upper())
+print(szoveg2)
+
+
+
+
+
+
+
+
+
+
+
 
 
 
